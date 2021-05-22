e3e5651d4b660e4fa77dc0b3d711b10b7b6a6845
Antin Williams <williamsantin43@gmail.com>
2021-05-22 18:18:00 +0200

    Initial Commit

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.androidproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidproject.ui.theme.AndroidProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +21,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun UnitConverter() {
+    val text = remember { mutableStateOf(TextFieldValue("Text")) }
+    Column() {
+        Row(){
+            OutlinedTextField(value = text.value,
+                onValueChange = { text.value = it },
+                label = { Text("Enter Unit") })
+
+        }
+        Spacer(
+            Modifier.padding(10.dp)
+        )
+        Row(){
+            val text = remember { mutableStateOf(TextFieldValue("Text")) }
+            OutlinedTextField(value = text.value,
+                onValueChange = { text.value = it },
+                label = { Text("Enter Unit") })
+
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AndroidProjectTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
